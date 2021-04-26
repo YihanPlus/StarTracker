3d34f4f39352ed14e6f18b29798d92b4fc2592e5
Hayley Huang <73251017+YihanPlus@users.noreply.github.com>
2021-04-26 03:38:34 -0700

    update satelliteList.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends React.Component {
         return (
             <footer className="footer">
                 @2021 Starlink Tracker. All Rights Reserved. Made with ❤ by Hayley.
+                Images Source: Unsplash & Freepick.
             </footer>
         );
     }
--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { Button } from 'antd';
+import {Button, Spin, List, Avatar, Checkbox} from 'antd';
+import satellite from '../assets/images/satellite.png'
 
 class SatelliteList extends React.Component {
     render() {
+        const satList = this.props.satInfo ? this.props.satInfo.above : [];
+        const {isLoad} = this.props.isLoading;
+
         return (
             <div className="sat-list-box">
                 <Button className="sat-list-btn"
-                        style={{textAlign: "center",
-                                background: "#0a2a56",
-                                color: "#fff",
-                                margin: "10px"}}>
+                        style={{
+                            textAlign: "center",
+                            background: "#0a2a56",
+                            color: "#fff",
+                            margin: "10px"
+                        }}>
                     Track on the map
                 </Button>
-                <hr color="#0a2a56" size="1" width="150px" />
-                <div>
-                    data
-                </div>
+                <hr color="#0a2a56" size="1" width="150px"/>
+
+                {
+                    isLoad ?
+                        <div className="spin-box">
+                            <Spin tip="Loading..." size="large"/>
+                        </div>
+                        :
+                        <List className="sat-list"
+                              itemLayout="horizontal"
+                              size="small"
+                              dataSource={satList}
+                              renderItem={(item) => (
+                                  <List.Item actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]}>
+                                      <List.Item.Meta
+                                          avatar={<Avatar size={50} src={satellite}/>}
+                                          title={<p>{item.satname}</p>}
+                                          description={`Launch Date: ${item.launchDate}`}
+                                      />
+                                  </List.Item>
+                              )}
+                        />
+                }
             </div>
         );
     }
